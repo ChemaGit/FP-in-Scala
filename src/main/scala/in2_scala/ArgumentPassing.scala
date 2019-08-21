29398f261924eb0016378fdbfaef58c38f2de4aa
Chema <chema@localhost.chema>
2019-08-21 17:16:34 +0200

    21082019

--- src/main/scala/in2_scala/ArgumentPassing.scala
+++ src/main/scala/in2_scala/ArgumentPassing.scala
@@ -2,6 +2,15 @@ package in2_scala
 
 object ArgumentPassing{
 	def main(args: Array[String]): Unit = {
-		
+		def zeroArray(a: Array[Int], i: Int): Unit = {
+			if(i < a.length){
+				a(i) = 0
+				zeroArray(a,i+1)
+			}
+		}
+		val arr = Array(1,2,3,4,5,6,7,8,9)
+		println(zeroArray(arr,0))
+		println(arr.mkString(","))	
 	}
+	
 }
---
+++ src/main/scala/in2_scala/Currying.scala
@@ -0,0 +1,7 @@
+package in2_scala
+
+object Currying {
+	def main(args: Array[String]): Unit = {
+
+	}
+}
