944a90b0972611889dcb00f46dffbb50d2c295a6
Chema <chema@localhost.chema>
2019-11-15 12:38:31 +0100

    15112019

---
+++ src/main/scala/scala_and_functional_programming/lectures/part2oop/AnonymousClasses.scala
@@ -0,0 +1,51 @@
+package lectures.part2oop
+
+object AnonymousClasses extends App {
+
+	abstract class Animal {
+		def eat: Unit
+	}
+
+	// anonymous class
+	val funnyAnimal: Animal = new Animal {
+		override def eat: Unit = println("ahahahahahahahahah")
+	}
+	/*
+		equivalent with
+
+		class AnonymousClasses$$anon$1 extends Animal {
+			override def eat: Unit = println("ahahahahahahahah")
+		}
+		val funnyAnimal: Animal = new AnonymousClasses$$anon$1
+	*/
+
+	println(funnyAnimal.getClass)
+
+	class Person(name: String) {
+		def hi: Unit = println(s"Hi, my name is $name, how can I help?")
+	}
+
+	val Jim = new Person("Jim") {
+		override def hi: Unit = println(s"Hi, my name is Jim, how can I be of service?")
+	}
+
+}
+/*
+TAKEAWAYS
+
+	- We can instantiate types and override fields of methods on the spot
+
+		trait Animal {
+			def eat: Unit
+		}
+		
+		val predator = new Animla {
+			override def eat: Unit = println("RAWR!")
+		}
+
+	- Rules
+		- Pass in required constructor arguments if needed
+		- Implement all abstract fields/methods
+
+	- WORKS FOR TRAITS AND CLASSES (abstract or not)
+*/
