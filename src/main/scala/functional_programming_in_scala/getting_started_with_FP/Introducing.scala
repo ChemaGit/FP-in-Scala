3ac61d9338fcf9ddf8c5182a219e716d599a776a
Chema <chema@localhost.chema>
2020-02-20 22:53:51 +0100

    20200220

--- src/main/scala/functional_programming_in_scala/getting_started_with_FP/Introducing.scala
+++ src/main/scala/functional_programming_in_scala/getting_started_with_FP/Introducing.scala
@@ -11,11 +11,6 @@ object Introducing {
     else n
   }
 
-  private def formatAbs(x: Int) = {
-    val msg = "The absoute value of %d is %d"
-    msg.format(x, abs(x))
-  }
-
   def factorial(x: Int): Int = {
     @annotation.tailrec
     def go(n: Int, acc: Int): Int = {
@@ -25,6 +20,29 @@ object Introducing {
     go(x, 1)
   }
 
+  private def formatAbs(x: Int) = {
+    val msg = "The absoute value of %d is %d"
+    msg.format(x, abs(x))
+  }
+
+  private def formatFactorial(n: Int) = {
+    val msg = "The factorial of %d is %d."
+    msg.format(n, factorial(n))
+  }
+
+  /**
+    * formatResult is a higher-order-function (HOF)
+    * formatAbs and formatFactorial, are almost identical.
+    * We can generalize these to a single function, formatResult.
+    * Which accepts as an argument a function to apply to its argument
+    * @param name
+    * @param n
+    * @param f: a function from Int to Int
+    */
+  def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d."
+    msg.format(name, n, f(n))
+  }
 
   def fibonacci(n: Int): Int = {
     if(n == 0) 0
@@ -60,13 +78,61 @@ object Introducing {
     }
   }
 
+  /**
+    * Monomorphic function to find a String in an array
+    */
+  def findFirst(ss: Array[String], key: String): Int = {
+    @annotation.tailrec
+    def loop(n: Int): Int = {
+      if(n >= ss.length) -1
+      else if(ss(n) == key) n
+      else loop(n + 1)
+    }
 
+    loop(0)
+  }
+
+  /**
+    * An example of a polymorphic function
+    * We can write findFirst more generally for any type A
+    * by accepting a function to use for testing a particular A value
+    * @param
+    */
+    def findFirst[A](as: Array[A], p: A => Boolean): Int = {
+      def loop(n: Int): Int = {
+        if(n >= as.length) -1
+        else if(p(as(n))) n
+        else loop(n + 1)
+      }
+
+      loop(0)
+    }
+
+  /**
+    * Check whether an Array[A] is sorted according to a
+    * given comparison function
+    * @param
+    */
+    def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+      //TODO: terminar
+      true
+    }
 
   def main(args: Array[String]): Unit = {
-    println(formatAbs(-42))
     println(factorial(5))
     println(fibonacci(10))
     println(fibonacciTailRec(10))
+    println()
+    println(formatAbs(-42))
+    println(formatFactorial(5))
+    println()
+
+    println(formatResult("absolute value", -42, abs))
+    println(formatResult("factorial", 7, factorial))
+    println(formatResult("fibonacci", 8, fibonacciTailRec))
+    println()
+
+    println(findFirst(Array(1,2,3,4,5,6,7,8), (x: Int) => x == 7 ))
   }
 
 }
--- src/main/scala/scala_and_functional_programming/lectures/part3fp/Sequences.scala
+++ src/main/scala/scala_and_functional_programming/lectures/part3fp/Sequences.scala
@@ -1,5 +1,7 @@
 package scala_and_functional_programming.lectures.part3fp
 
+import scala.util.Random
+
 object Sequences extends App {
   // Seq
   val aSequence = Seq(2,4,1,3)
@@ -10,4 +12,68 @@ object Sequences extends App {
   println(aSequence.sorted)
 
   // Ranges
+  val aRange: Seq[Int] = 1 to 10 // instead of to(inclusive) until(exclusive)
+  aRange.foreach(println)
+
+  // List
+  val aList = List(1,2,3)
+  val prepended = 42 :: aList
+  println(prepended)
+  val preapend = 42 +: aList :+ 89
+  println(preapend)
+
+  val apples5 = List.fill(5)("apple")
+  println(apples5)
+  println(aList.mkString("[","-","]"))
+
+  // Arrays
+  val numbers = Array(1,2,3,4)
+  val threeElements = Array.ofDim[Int](3)
+  println(threeElements)
+  threeElements.foreach(println)
+
+  // mutation
+  numbers(2) = 0 // syntax sugar for numbers.update(2,0)
+  println(numbers.mkString(" "))
+
+  // arrays and seq
+  val numbersSeq: Seq[Int] = numbers // implicit conversion
+  println(numbersSeq)
+
+  // vectors
+  val vector: Vector[Int] = Vector(1,2,3)
+  println(vector)
+
+  // vectors vs lists
+
+  val maxRuns = 1000
+  val maxCapacity = 1000000
+
+  /**
+    * Benchmarking between Lists and Vectors
+    * @param collection
+    * @return
+    */
+  def getWriteTime(collection: Seq[Int]): Double = {
+    val r = new Random
+    val times = for {
+      it <- 1 to maxRuns
+    } yield {
+      val currentTime = System.nanoTime()
+      collection.updated(r.nextInt(maxCapacity), r.nextInt()) // operation
+      System.nanoTime() - currentTime
+    }
+    times.sum * 1.0 / maxRuns
+  }
+
+  val numberList = (1 to maxCapacity).toList
+  val numbersVector = (1 to maxCapacity).toVector
+
+  // keeps reference to tail
+  // updating an element in the middle takes long time
+  println(getWriteTime(numberList))
+  // depth of the tree is small
+  // needs to replace an entire 32-element chunk
+  println(getWriteTime(numbersVector))
+
 }
