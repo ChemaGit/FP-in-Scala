6699dea88350e540d71dc6f05a80f26993ecec6b
Chema <chema@localhost.chema>
2020-07-23 19:49:14 +0200

    23-07-2020

--- src/main/scala/hands_on_scala/basic_scala/MethodsAndFunctions.scala
+++ src/main/scala/hands_on_scala/basic_scala/MethodsAndFunctions.scala
@@ -14,8 +14,18 @@ object MethodsAndFunctions {
     "hello " + i
   }
 
+  class Box(var x: Int) {
+    def update(f: Int => Int) = x = f(x)
+    def printMsg(msg: String) = {
+      println(msg + x)
+    }
+  }
+
   def main(args: Array[String]): Unit = {
 
+    /**
+      * METHODS
+      */
     printHello(1)
     printHello(times = 2) // argument name provided explicitly
 
@@ -24,5 +34,48 @@ object MethodsAndFunctions {
     println(hello())
     val helloHello = hello(123) + " " + hello(456)
     println(helloHello.reverse)
+
+    /**
+      * FUNCTION VALUES
+      */
+    var g: Int => Int = i => i + 1
+    println(g(15))
+
+    g = i => i * 2
+    println(g(15))
+
+    /**
+      * Methods taking functions
+      */
+    val b = new Box(1)
+    b.printMsg("Hello")
+    b.update(i => i + 5)
+    b.printMsg("Hello")
+
+    b.update(i => i * 5)
+    b.printMsg("Hello")
+
+    b.update(_ + 5)
+    b.printMsg("Hello")
+
+    def increment(i: Int) = i + 1
+    val c = new Box(123)
+    c.update(increment) // Providing a method reference
+    c.printMsg("Hello")
+    c.update(x => increment(x)) // Explicitly writing out the lambda
+    c.printMsg("Hello")
+    c.update{x => increment(x)} // Methods that take a single lambda can be called with {}s
+    c.printMsg("Hello")
+    c.update(increment(_)) // You can also use the `_` placeholder syntax
+    c.printMsg("Hello")
+
+
+    def myLoop(start: Int, end: Int)(callback: Int => Unit) = {
+        for (i <- Range(start, end)) {
+          callback(i)
+        }
+    }
+
+    myLoop(start = 5, end = 10) { i => println(s"i has value ${i}")}
   }
 }
